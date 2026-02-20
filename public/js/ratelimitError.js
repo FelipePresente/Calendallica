ecff71c8be22aaa9580d645d9a360cbc9fe90771
Felipe Presente <felipe.presente@aluno.ifsp.edu.br>
2026-02-20 00:26:55 -0300

    removed public folder

--- public/js/ratelimitError.js
+++
@@ -1,9 +0,0 @@
-export default async function ratelimitError(response) {
-    if (response.status === 429) {
-        const data = await response.json()
-        alert(data.message)
-        return true
-    }
-
-    return false
-}
