9cdace878709fadeeb9dee1271b0c09929ec5093
Felipe Presente <felipe.presente@aluno.ifsp.edu.br>
2026-03-01 15:06:08 -0300

    past days on calendar are a little bit darker

--- client/src/components/CalendarDayPast.tsx
+++ client/src/components/CalendarDayPast.tsx
@@ -1,6 +1,6 @@
 export default function CalendarDayPast({ day }: { day: number }) {
     return (
-        <div className="bg-zinc-950/20 border border-zinc-900/50 aspect-square rounded-lg flex items-center justify-center text-zinc-800 font-semibold text-sm select-none">
+        <div className="bg-zinc-950/40 border border-zinc-900/50 aspect-square rounded-lg flex items-center justify-center text-zinc-800 font-semibold text-sm select-none">
             {day}
         </div>
     )
