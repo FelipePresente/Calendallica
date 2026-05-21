ff3fc9b9c0cff8983207d91cd301a06b883b7125
Felipe Presente <felipe.presente@outlook.com>
2026-05-21 16:58:39 -0300

    modified cookies

--- server/src/main/java/calendallica_server/auth/CookieService.java
+++ server/src/main/java/calendallica_server/auth/CookieService.java
@@ -1,27 +1,34 @@
 package calendallica_server.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CookieService {
+    @Value("${api.security.cookie.secure:false}")
+    private boolean cookieSecure;
+
+    @Value("${api.security.cookie.samesite:Strict}")
+    private String cookieSameSite;
+
     public ResponseCookie generateCookie(String token) {
         return ResponseCookie.from("access_token", token)
                 .httpOnly(true)
-                .secure(false)
+                .secure(cookieSecure)
                 .path("/")
                 .maxAge(60 * 60 * 24 * 14)
-                .sameSite("Strict")
+                .sameSite(cookieSameSite)
                 .build();
     }
 
     public ResponseCookie deleteCookie() {
         return ResponseCookie.from("access_token", null)
                 .httpOnly(true)
-                .secure(false)
+                .secure(cookieSecure)
                 .path("/")
                 .maxAge(0)
-                .sameSite("Strict")
+                .sameSite(cookieSameSite)
                 .build();
     }
 }
