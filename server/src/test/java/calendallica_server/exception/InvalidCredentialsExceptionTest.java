b94c2c2e4ab6494b1d831b49d65416af28da520b
FELIPE DE OLIVEIRA PRESENTE <CG3037797@car.ifsp>
2026-09-23 09:22:59 -0300

    testing tests

---
+++ server/src/test/java/calendallica_server/exception/InvalidCredentialsExceptionTest.java
@@ -0,0 +1,18 @@
+package calendallica_server.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class InvalidCredentialsExceptionTest {
+    
+    @Test
+    void shouldFail() {
+        Throwable exception = assertThrows(InvalidCredentialsException.class, () -> {
+            throw new InvalidCredentialsException("Invalid credentials");
+        });
+
+        assertEquals("Invalid credential", exception.getMessage());
+    }
+}
