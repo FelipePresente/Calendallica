abff4c8b7af9854e2eeb5075b9431bbc1a6186b3
Felipe Presente <felipe.presente@aluno.ifsp.edu.br>
2026-09-25 10:28:35 -0300

    Fix expected exception message in test

--- server/src/test/java/calendallica_server/exception/InvalidCredentialsExceptionTest.java
+++ server/src/test/java/calendallica_server/exception/InvalidCredentialsExceptionTest.java
@@ -13,6 +13,6 @@ public class InvalidCredentialsExceptionTest {
             throw new InvalidCredentialsException("Invalid credentials");
         });
 
-        assertEquals("Invalid credentials", exception.getMessage());
+        assertEquals("Invalid credential", exception.getMessage());
     }
 }
